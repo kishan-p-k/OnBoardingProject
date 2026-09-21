85dd73799a5ea88d42dd197a6d1b260244058fa8
SOTI\asunil <athulya.sunil@soti.net>
2026-09-21 12:14:24 +0530

    Added authguard

--- Frontend/bug-tracker-client/src/app/app.routes.ts
+++ Frontend/bug-tracker-client/src/app/app.routes.ts
@@ -4,14 +4,19 @@ import { BugListComponent } from './Components/bug-list/bug-list';
 import { BugComponent } from './Components/bug-component/bug-component';
 import { UserBugsComponent } from './Components/user-bugs/user-bugs';
 import { CreateBugComponent } from './Components/create-bug/create-bug';
+import { authGuard } from './auth-guard'
 
 
 export const routes: Routes = [
   { path: 'login', component: LoginPage },
-  { path: 'bug/create', component: CreateBugComponent },
 
-  { path: 'bug/:ref_id', component: BugComponent },
-  { path: 'bug', component: BugListComponent },
-  { path: 'userbugs/:ref_id', component: UserBugsComponent },
+  { path: 'bug/create', component: CreateBugComponent, canActivate: [authGuard] },
+
+  { path: 'bug/:ref_id', component: BugComponent, canActivate: [authGuard] },
+
+  { path: 'bug', component: BugListComponent, canActivate: [authGuard] },
+
+  { path: 'userbugs/:ref_id', component: UserBugsComponent, canActivate: [authGuard] },
+
   { path: '**', redirectTo: '/login', pathMatch: 'full' }
 ];
---
+++ Frontend/bug-tracker-client/src/app/auth-guard.ts
@@ -0,0 +1,25 @@
+import { inject } from '@angular/core';
+import { CanActivateFn, Router } from '@angular/router';
+import { LoginPageService } from './Services/login-page-service';
+
+export const authGuard: CanActivateFn = (route, state) => {
+
+  const loginService = inject(LoginPageService);
+  const router = inject(Router);
+
+  const user = loginService.getCurrentUser();
+
+  if (!user) {
+    return router.createUrlTree(['/login']);
+  }
+
+  if (route.routeConfig?.path === 'userbugs/:ref_id') {
+    const refId = route.paramMap.get('ref_id');
+
+    if (refId !== user.reference_id) {
+      return router.createUrlTree(['/bug']);
+    }
+  }
+
+  return true;
+};
